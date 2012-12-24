6636eebb8abb8f1ec1afc83cdfd9f82132ab564f
ryosuke <ryosuke.pic@gmail.com>
2012-12-25 00:47:02 +0900

    update

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
-﻿?<?php
+﻿<?php
 
 require_once 'model.php';
 
 $uri = $_SERVER['REQUEST_URI'];
 if ($uri == '/index.php' || $uri == '/' || isset($_GET['state'])) {
-    list_friends();
-} elseif ($uri === 'getData' && isset($_GET['user_id'] && isset($_GET['lat1'] && isset($_GET['lon1'] && isset($_GET['lat2'] && isset($_GET['lon2'])) )))) {
-	//res_data();
-	$res = array();
-	$res['lat'] = 45.00;
-	$res['lon'] = 140.00;
-	$res['name'] = 'hoge';
-	$res['message'] = 'test';
+    //list_friends();
+	set_data();
+} elseif (isset($_GET['user_id']) && isset($_GET['lat1']) && isset($_GET['lon1']) && isset($_GET['lat2']) && isset($_GET['lon2'])) {
+	$res = get_data($_GET['user_id'], $_GET['lat1'], $_GET['lon1'], $_GET['lat2'], $_GET['lon2']);
 	$res = json_encode($res);
 	echo $res;
 } elseif (isset($_GET['dest_id'])) {
