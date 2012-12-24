033dcc80e331567fc9a44f822fcbf4be4a2dba5a
ryosuke <ryosuke.pic@gmail.com>
2012-12-24 15:42:42 +0900

    update

--- index.php
+++ index.php
@@ -5,6 +5,15 @@ require_once 'model.php';
 $uri = $_SERVER['REQUEST_URI'];
 if ($uri == '/index.php' || $uri == '/' || isset($_GET['state'])) {
     list_friends();
+} elseif ($uri === 'getData' && isset($_GET['user_id'] && isset($_GET['lat1'] && isset($_GET['lon1'] && isset($_GET['lat2'] && isset($_GET['lon2'])) )))) {
+	//res_data();
+	$res = array();
+	$res['lat'] = 45.00;
+	$res['lon'] = 140.00;
+	$res['name'] = 'hoge';
+	$res['message'] = 'test';
+	$res = json_encode($res);
+	echo $res;
 } elseif (isset($_GET['dest_id'])) {
     http_to_friends($_GET['src_id'], $_GET['dest_id']);
 } else {
